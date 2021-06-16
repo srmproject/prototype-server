30b2edfe83f983bb0fbe375884baa2753e79fb54
choisungwook <kgg1959@naver.com>
2021-06-16 21:42:50 +0900

    namespace 설명 변경

--- apis/argocd/namespace.py
+++ apis/argocd/namespace.py
@@ -3,11 +3,11 @@ from logger.log import log
 from .service import JenkinsTriggerJob
 from werkzeug.utils import redirect
 
-ns = Namespace('argo', version="1.0", description='argo controller')
+ns = Namespace('argocd', version="1.0", description='argocd controller')
 
 @ns.route("/healthcheck")
 class Index(Resource):
     @ns.doc(response={200: "success"})
     def get(self):
-        return "This is a argo health check api"
+        return "This is a argocd health check api"
 
