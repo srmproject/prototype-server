72ad19cb9612eb5b98247c06d33422e546cda6f9
choisungwook <kgg1959@naver.com>
2021-06-18 17:02:26 +0900

    app 생성을 함수로 변경

--- app.py
+++ app.py
@@ -5,17 +5,20 @@ from login.loginmanager import login_manager
 from db.db import db
 from flask_migrate import Migrate
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'thisisdemo'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
-app.config['TESTING'] = False
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'thisisdemo'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['TESTING'] = False
 
-# bludeprint 초기화
-app.register_blueprint(api_v1)
+    # bludeprint 초기화
+    app.register_blueprint(api_v1)
 
-# 데이터베이스 초기화
-db.init_app(app)
-migrate = Migrate(app, db)
+    # 데이터베이스 초기화
+    db.init_app(app)
+    migrate = Migrate(app, db)
 
-# Loginmanager 초기화
-login_manager.init_app(app)
+    # Loginmanager 초기화
+    login_manager.init_app(app)
+
+    return app
--- run.py
+++ run.py
@@ -1,5 +1,7 @@
-from app import app
+from app import create_app
 
 # run flask on localhost 
 if __name__=="__main__":
+    app = create_app()
     app.run(host='127.0.0.1', port=8888, debug=True)
+    
