2417e4d66c766de6902a3f6cdf9cf2abfee68b9e
choisungwook <kgg1959@naver.com>
2021-06-19 13:28:34 +0900

    테스트 스케쥴러 추가

--- scheduler/tasks.py
+++ scheduler/tasks.py
@@ -1,12 +1,20 @@
 from .create import scheduler
+from threading import Lock
 
 @scheduler.task(
     "interval",
     id="argocd controller",
-    seconds=1,
+    seconds=2,
     max_instances=1,
 )
 def task1():
     with scheduler.app.app_context():
         pass
+    
+        # test code
+        # config = scheduler.app.config
+        # lock = config['ARGOCD_EVENT_LOCK']
+        # lock.acquire()
+        # print("hello")
+        # lock.release()
         # print(scheduler.app.config)
